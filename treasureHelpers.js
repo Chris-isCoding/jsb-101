ead4ae26b07cc61d934fda3996a8f15ba885b1fa
ssheikhi <fx.sheikhi@gmail.com>
2024-10-31 15:46:00 +0100

    Add function to clear announcement restart game

--- treasureHelpers.js
+++ treasureHelpers.js
@@ -22,9 +22,21 @@ function isTree(event) {
 // function to make announcement when the treasure is found
 function makeAnnouncement(counter) {
   const announcement = document.body.appendChild(document.createElement('div'));
+  announcement.setAttribute("id", "announcement-div");
 
   announcement.innerText = `Bravo!! You found the treasure after ${counter} steps`;
   announcement.style.fontSize = '2rem';
 }
+function clearAnnouncement(){
+  let announcement = document.getElementById('announcement-div');
+    
+  // Check if the announcement element exists
+  if (announcement) {
+    announcement.removeAttribute('id')
+    announcement.innerText = '';
+  } else {
+    console.log("Announcement element not found.");
+  }
+}
 
-export { isTreasure, isTree, makeAnnouncement };
+export { isTreasure, isTree, makeAnnouncement, clearAnnouncement};
